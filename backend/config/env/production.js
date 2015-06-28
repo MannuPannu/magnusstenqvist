ee1ab643aeeb5d94c5afb36fa590cd621b0ebed3
Magnus Stenqvist <magnus.p.stenqvist@gmail.com>
2015-06-28 18:13:05 +0200

    foo

--- backend/config/env/production.js
+++ backend/config/env/production.js
@@ -3,7 +3,7 @@
 module.exports = {
     env: 'production',
     mongo: {
-		uri: 'mongodb://$OPENSHIFT_MONGODB_DB_HOST:$OPENSHIFT_MONGODB_DB_PORT/' 
+		uri: 'mongodb://$OPENSHIFT_MONGODB_DB_HOST:$OPENSHIFT_MONGODB_DB_PORT/',
         options: {
             server: {
                 auto_reconnect: true,
