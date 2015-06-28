308bfe2ef016cb315a32883a74b2371d6f3871e2
Magnus Stenqvist <magnus.p.stenqvist@gmail.com>
2015-06-28 18:24:21 +0200

    foo

--- backend/config/env/production.js
+++ backend/config/env/production.js
@@ -1,9 +1,21 @@
 ﻿'use strict';
 
+var connection_string = '127.0.0.1:27017/YOUR_APP_NAME';
+// if OPENSHIFT env variables are present, use the available connection info:
+if(process.env.OPENSHIFT_MONGODB_DB_PASSWORD){
+
+	  connection_string = process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +
+	    process.env.OPENSHIFT_MONGODB_DB_PASSWORD + "@" +
+	      process.env.OPENSHIFT_MONGODB_DB_HOST + ':' +
+	        process.env.OPENSHIFT_MONGODB_DB_PORT + '/' +
+	          process.env.OPENSHIFT_APP_NAME;
+
+}:
+
 module.exports = {
     env: 'production',
     mongo: {
-		uri: 'mongodb://$OPENSHIFT_MONGODB_DB_HOST:$OPENSHIFT_MONGODB_DB_PORT/',
+		uri: 'mongodb://' + connection_string ,
         options: {
             server: {
                 auto_reconnect: true,
